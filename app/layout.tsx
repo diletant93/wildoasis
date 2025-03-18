ee70caa2893c2c95a8e108f1116db8ebcf11fcc9
Artur <artur.talimonchyk@gmail.com>
2025-03-18 20:16:24 +0100

    home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body className={cn('bg-primary-950 text-primary-100 min-h-screen flex flex-col', josefin.className)}>
+      <body className={cn('bg-primary-950 text-primary-100 min-h-screen flex flex-col relative', josefin.className)}>
         <Header />
         <div className="flex-1  px-8 py-12">
           <main className="max-w-7xl mx-auto">
