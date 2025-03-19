e5e499a294f41534eb488119be935b108c099a8b
Artur <artur.talimonchyk@gmail.com>
2025-03-19 22:35:33 +0100

    added the active state of a filter item

--- app/_components/Filter.tsx
+++ app/_components/Filter.tsx
@@ -2,32 +2,29 @@
 import { useState } from "react";
 import { FilterOption } from "../_types/filter";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { filterOptions } from "../_constants/filter";
 
 export default function Filter() {
     const [option, setOption] = useState<FilterOption>('all')
     const searchParams = useSearchParams()
     const router = useRouter()
     const pathname = usePathname()
-    function handleFilter(filter:FilterOption){
+
+    const activeFilter = searchParams.get('capacity') ?? 'all' as FilterOption
+    function handleFilter(filter: FilterOption) {
         setOption(filter)
         const params = new URLSearchParams(searchParams)
         params.set('capacity', filter)
-        router.replace(`${pathname}?${params.toString()}`,{scroll:false})
+        router.replace(`${pathname}?${params.toString()}`, { scroll: false })
     }
+
     return (
-        <div className="flex gap-3   my-5 border border-primary-700">
-            <button onClick={()=>handleFilter('all')} className="px-4 py-3 hover:bg-primary-900 cursor-pointer">
-                All cabins
-            </button>
-            <button onClick={()=>handleFilter('small')}  className="px-4 py-3 hover:bg-primary-900 cursor-pointer">
-                1&mdash;3
-            </button>
-            <button onClick={()=>handleFilter('medium')}  className="px-4 py-3 hover:bg-primary-900 cursor-pointer">
-                4&mdash;7
-            </button>
-            <button onClick={()=>handleFilter('large')}  className="px-4 py-3 hover:bg-primary-900 cursor-pointer">
-                8&mdash;12
-            </button>
+        <div className="flex  my-5 border border-primary-700">
+            {filterOptions.map(option => (
+                <button key={option.value} onClick={() => handleFilter(option.value)} className={`px-4 py-3 hover:bg-primary-700 cursor-pointer ${option.value === activeFilter && 'bg-primary-700'}`}>
+                    {option.label}
+                </button>
+            ))} 
         </div>
     );
 }
--- app/_constants/filter.ts
+++ app/_constants/filter.ts
@@ -1,3 +1,8 @@
 import { FilterOption } from "../_types/filter";
 
-export const filterOptions: FilterOption[] = ['small', 'medium', 'large', 'all'];
+export const filterOptions: { label: string; value: FilterOption }[] = [
+    { label: 'All cabins', value: 'all' },
+    { label: '1-3 guests', value: 'small' },
+    { label: '3-8 guests', value: 'medium' },
+    { label: '8-12 guests', value: 'large' },
+  ];
