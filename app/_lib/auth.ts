e1f09dfbca9c82e805c448ce240cd2270c11e161
Artur <artur.talimonchyk@gmail.com>
2025-03-20 22:28:29 +0100

    commit;

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -9,10 +9,14 @@ const authConfig:NextAuthConfig ={
             clientId:process.env.NEXT_PRIVATE_GOOGLE_CLIENT_ID,
             clientSecret:process.env.NEXT_PRIVATE_GOOGLE_CLIENT_SECRET
         })
-    ]
+    ],
+    callbacks:undefined,
+    pages:undefined,
 }
 
 export const {
     auth,
+    signIn,
+    signOut,
     handlers:{GET,POST}
 } = NextAuth(authConfig)
