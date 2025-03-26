98e12350b85f860210384cc64c67fc05620e110e
Artur <artur.talimonchyk@gmail.com>
2025-03-26 10:50:30 +0100

    fixed the sign out and sign in buttons

--- app/_components/SignOutButton.tsx
+++ app/_components/SignOutButton.tsx
@@ -1,9 +1,16 @@
+"use client"
 import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/solid';
-import { signOutAction } from '../_actions/authActions';
+import { signInAction, signOutAction } from '../_actions/authActions';
+import { useActionToast } from '../_hooks/useActionToast';
 
 function SignOutButton() {
+  const actionToast = useActionToast()
+  async function handleSignIn() {
+    const response = await signOutAction()
+    actionToast(response)
+  }
   return (
-    <form action={signOutAction}>
+    <form action={handleSignIn}>
       <button className='py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full cursor-pointer'>
         <ArrowRightOnRectangleIcon className='h-5 w-5 text-primary-600' />
         <span>Sign out</span>
