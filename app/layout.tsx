c2ac73ae439a15fca04fecd595ed503d0807b225
Artur <artur.talimonchyk@gmail.com>
2025-03-29 14:29:15 +0100

    commit for redeploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-import { Metadata } from "next";
+import { Metadata, Viewport } from "next";
 import './globals.css'
 import { Josefin_Sans } from 'next/font/google';
 import { cn } from "./_utils/cn";
 import Header from "./_components/Header";
 import Providers from "./_providers/Providers";
-import { Toaster } from "@/components/ui/sonner";
-import ScreenSizeWidget from "./_components/ScreenSizeWidget";
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -18,11 +16,12 @@ export const metadata: Metadata = {
     default: 'The Wild Oasis'
   },
   description: 'Discover and book the most luxurious hotels worldwide, offering unparalleled comfort, elegance, and top-tier hospitality.',
-  viewport:{
-    width:'device-width',
-    initialScale:1.0,
-  }
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+}
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
