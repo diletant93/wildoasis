7b6f229437793b149761b89079214a3423dfe355
Artur <artur.talimonchyk@gmail.com>
2025-04-10 22:11:47 +0200

    some stylying enchancements of the menu on mobile devices

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from '@/app/_components/Logo';
 
 function Header() {
   return (
-    <header className='border-b border-primary-900 px-8 py-5 order-1 sm:order-0'>
+    <header className='border-primary-700 px-8 py-4 order-1 sm:order-0 border-t sm:border-t-0 sm:border-b sm:py-5'>
       <div className='flex sm:justify-between items-cente max-w-[80rem] 2xl:max-w-[100rem] mx-auto z-[100]'>
         <Logo />
         <Navigation />
