7c7139a91bd818af45a1b4c8ff0c104632a007aa
Artur <artur.talimonchyk@gmail.com>
2025-04-10 21:55:51 +0200

    again trying to fix

--- app/_hooks/useViewportHeight.ts
+++ app/_hooks/useViewportHeight.ts
@@ -8,25 +8,6 @@ export function useViewportHeight(){
             const vh = window.innerHeight * 0.01;
             document.documentElement.style.setProperty('--vh', `${vh}px`);
         }
-        
         updateHeight();
-        
-        window.addEventListener('resize', updateHeight);
-        window.addEventListener('orientationchange', updateHeight);
-        
-        if(window.visualViewport){
-            window.visualViewport.addEventListener('resize', updateHeight);
-            window.visualViewport.addEventListener('scroll', updateHeight); 
-        }
-        
-        return () => {
-            window.removeEventListener('resize', updateHeight);
-            window.removeEventListener('orientationchange', updateHeight);
-            
-            if(window.visualViewport){
-                window.visualViewport.removeEventListener('resize', updateHeight);
-                window.visualViewport.removeEventListener('scroll', updateHeight);
-            }
-        };
     }, []);
 }
